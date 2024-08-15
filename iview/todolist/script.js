fc1f4153789121b92155b7b0a73a637ccc4847e5
Sukantongithub <sukantravi005@gmail.com>
2024-08-15 07:51:25 +0530

    js file updated

--- iview/todolist/script.js
+++ iview/todolist/script.js
@@ -1,7 +1,15 @@
-var input=document.getElementsByClassName("inputtxt")
+var input=document.getElementById("intxt")
 const btn=document.querySelector(".addbtn")
 const list=document.querySelector(".task")
+const state =document.getElementById("taskno")
+const dels=document.querySelectorAll("#trash")
 btn.addEventListener('click',()=>{
+    list.innerHTML="<div class='addtask'><h2 class='inval'>"+input.value+"</h2><i class='bx bxs-trash-alt' id='trash'></i></div>";
 
-    list.innerHTML=input.value + "<button>Delete</button>";
+    state.textContent=count+" Task to be done!";
+
 })
+var count=count("");
+// dels.addEventListener('click',()=>{
+//     dels.forEach(del=>{div.remove;})
+// })
